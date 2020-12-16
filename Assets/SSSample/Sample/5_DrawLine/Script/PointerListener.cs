f7799b18d7f57106fb84b71bd012ab88c1889402
ashzeroh <75300716+ashzeroh@users.noreply.github.com>
2020-12-16 22:32:34 +0900

    Revert "edit comment"
    This reverts commit 3a39d3ac982848c8a8869e0a21e7de4ba3ce91bd.

--- Assets/SSSample/Sample/5_DrawLine/Script/PointerListener.cs
+++ Assets/SSSample/Sample/5_DrawLine/Script/PointerListener.cs
@@ -6,8 +6,8 @@ using UnityEngine.EventSystems;
 //버튼에 추가되어서 사용되어진다.
 public class PointerListener : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    public DrawLine _drawLineST; //DrawLine 스크립트를 참조하기 위함.
-    bool _pressed = false;  //버튼이 눌렸는지 확인하기 위함.
+    public DrawLine _drawLineST;
+    bool _pressed = false; 
     public void OnPointerDown(PointerEventData eventData) //해당 버튼이 눌렸을때.
     {
         _pressed = true; 
@@ -23,9 +23,7 @@ public class PointerListener : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     {
         if (_pressed)
         {
-            //버튼이 눌려져 있는 동안 
-            //참조된 DrawLine 스크립트의
-            //StartDrawLine()을 호출해와 선을 긋는다.
+            //버튼이 눌려져 있는 동안 선을 긋는다.
             _drawLineST.StartDrawLine();
         }
     }
